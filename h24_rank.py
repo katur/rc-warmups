d4dae6f9d556c868a993fec110af7ac36501fc4b
Katherine Erickson <katherine.erickson@gmail.com>
2016-08-24 11:12:25 -0400

    Add today's prompts

---
+++ h24_rank.py
@@ -0,0 +1,22 @@
+'''Write a function that takes a filename and a character, and prints the lines
+in the file, ordered by the number of times the character appears in each line.
+
+E.g.
+rank('foo.txt', 'e') ->
+    how's everyone doing?
+    hey,
+    hello world!
+
+foo.txt
+    hey,
+    hello world!
+    how's everyone doing?
+'''
+
+
+def rank(filename, char):
+    pass
+
+
+if __name__ == '__main__':
+    rank('foo.txt', 'e')
---
+++ h24_swap.py
@@ -0,0 +1,15 @@
+'''Write a function that takes two lists: L1 contains arbitrary data, L2
+contains pairs of numbers. For each pair in L2, swap the corresponding elements
+in L1, and return the result at the end.
+
+E.g.
+swap(['a', 'b', 'c'], [(0, 2), (1, 2)]) -> ['c', 'a', 'b']
+'''
+
+
+def swap(l1, l2):
+    pass
+
+
+if __name__ == '__main__':
+    print swap(['a', 'b', 'c'], [(0, 2), (1, 2)])
