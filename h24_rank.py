588f14264461143b4b23d0592ed5e5884bc76f68
Katherine Erickson <katherine.erickson@gmail.com>
2016-08-24 11:24:56 -0400

    Add solution to rank

--- h24_rank.py
+++ h24_rank.py
@@ -13,10 +13,31 @@ foo.txt
     how's everyone doing?
 '''
 
+import re
+
 
 def rank(filename, char):
-    pass
+    count_to_lines = {}
+
+    with open(filename, 'r') as f:
+        for line in f:
+            count = get_count(char, line)
+
+            if count not in count_to_lines:
+                count_to_lines[count] = [line]
+            else:
+                count_to_lines[count].append(line)
+
+    for count, lines in sorted(count_to_lines.items(), reverse=True):
+        for line in lines:
+            # Since print adds a newline, remove the newline already there
+            print line[:-1]
+
+
+def get_count(char, s):
+    """Count number of char in string s."""
+    return sum(1 for match in re.finditer(char, s))
 
 
 if __name__ == '__main__':
-    rank('foo.txt', 'e')
+    rank('h24_foo.txt', 'e')
