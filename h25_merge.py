f1b5b058ac7cec3b2bd56a19db26960ecea1ff5f
Katherine Erickson <katherine.erickson@gmail.com>
2016-08-25 15:15:33 -0400

    Add today's prompts

---
+++ h25_merge.py
@@ -0,0 +1,15 @@
+'''Write a function that takes a list of sorted lists of numbers, and returns a
+single sorted list with all of the numbers. Don't use your language's sort
+function.
+
+E.g.
+merge([[1, 5, 7], [3, 4, 9]]) -> [1, 3, 4, 5, 7, 9]
+'''
+
+
+def merge(l):
+    pass
+
+
+if __name__ == '__main__':
+    print merge([[1, 5, 7], [3, 4, 9]])
---
+++ h25_splitby.py
@@ -0,0 +1,24 @@
+'''Write a function that takes a filename and a list of words, and returns a
+list of strings that appear in the file between occurrences of the words in the
+list.
+
+E.g.
+splitby('foo.txt', ['am', 'is']) -> [
+    'this',
+    'an\nexample- I\n',
+    'excited!'
+]
+
+foo.txt
+    this is an
+    example- I
+    am excited!
+'''
+
+
+def splitby(filename, l):
+    pass
+
+
+if __name__ == '__main__':
+    print splitby('foo.txt', ['am', 'is'])
